f8f8a34c29469c46766174cba21da3bb7f1bd423
ndavido <D00239107@student.dkit.ie>
2023-04-01 14:41:35 +0300

    :white_check_mark: Applied Relation between tables

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 Base = declarative_base()
 engine = create_engine("sqlite:///todo.db",echo=False, future = True)
@@ -8,8 +10,12 @@ engine = create_engine("sqlite:///todo.db",echo=False, future = True)
 class Item(Base):
     __tablename__ = "items"
     itemId = Column(Integer, primary_key = True, autoincrement = True)
+    owner = Column(String, ForeignKey("users.name"))
     name = Column(String, nullable = False)
     description = Column(String, nullable = True)
+    timeStamp = Column(DateTime, nullable = False, default = datetime.utcnow)
+    users = relationship("User", back_populates="items")
+    
     
 class User(Base):
     __tablename__ = "users"
@@ -17,6 +23,8 @@ class User(Base):
     email = Column(String, nullable = False)
     name = Column(String, nullable = False)
     password = Column(String, nullable = False)
+    itemId = Column(Integer, ForeignKey("items.itemId"))
+    items = relationship("Item", back_populates="users")
     
 Base.metadata.create_all(engine)
 
