ba648a459f5475e47efc5924358c03ca42df77aa
ndavido <D00239107@student.dkit.ie>
2023-04-17 13:12:47 +0300

    :white_check_mark: Updated the database structure

--- database.py
+++ database.py
@@ -4,7 +4,7 @@
 #
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, DateTime
+from sqlalchemy import Boolean, Column, Integer, String, create_engine, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
@@ -15,20 +15,24 @@ engine = create_engine("sqlite:///todo.db",echo=False, future = True)
 class Item(Base):
     __tablename__ = "items"
     itemId = Column(Integer, primary_key = True, autoincrement = True)
-    owner = Column(String, ForeignKey("users.username"))
+    
+    owner = Column(String, ForeignKey("users.userId"))
     name = Column(String, nullable = False)
     description = Column(String, nullable = True)
     timeStamp = Column(DateTime, nullable = False, default = datetime.utcnow)
-    # share = Column(String, ForeignKey("users.username"), nullable=True)
+    sharedUser = Column(String, ForeignKey("users.userId"), nullable=True)
+    
     users = relationship("User", secondary="useritem", back_populates="items", single_parent = True)
     
 # Users  
 class User(Base):
     __tablename__ = "users"
     userId = Column(Integer, primary_key = True, autoincrement = True)
+    
     username = Column(String, nullable = False)
     password = Column(String, nullable = False)
-    # sharedNotes = Column(Integer, ForeignKey("items.itemId"))
+    sharedNotes = Column(Integer, ForeignKey("items.itemId"))
+    
     items = relationship("Item", secondary="useritem", back_populates="users", single_parent = True)
 
 # User Items
@@ -36,7 +40,7 @@ class UserItem(Base):
     __tablename__ = "useritem"
     userId = Column(Integer, ForeignKey("users.userId"), primary_key = True)
     itemId = Column(Integer, ForeignKey("items.itemId"), primary_key = True)
-    # sharedUserId = Column(Integer, ForeignKey("items.share"), primary_key = True, nullable = True)
+    is_owner = Column(Boolean, nullable=False, default=False)
     
 Base.metadata.create_all(engine)
 
